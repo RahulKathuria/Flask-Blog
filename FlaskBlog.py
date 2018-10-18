119773d5ab2a9059c33a5abc9c6920e1cfb0f776
RahulKathuria <rahulkathuria52@gmail.com>
2018-10-17 20:00:00 -0600

    Added Posts Data

--- FlaskBlog.py
+++ FlaskBlog.py
@@ -1,6 +1,21 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
+
+posts = [
+    {
+        'author': 'Rahul Kathuria',
+        'title': 'Blog Post 1',
+        'content': 'First Post content',
+        'date': 'September 20, 2018'
+    },
+    {
+        'author': 'Chitkara',
+        'title': 'Blog Post 2',
+        'content': 'Second Post content',
+        'date': 'September 21, 2018'
+    }
+]
 @app.route("/")
 @app.route("/home")
 def home():
@@ -8,7 +23,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
